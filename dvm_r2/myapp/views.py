5ff6ea7ac16e68b10613fa905e1d881fc2eda3b8
ayush-gupta-dev <ayushguptadev1@gmail.com>
2024-01-05 01:58:27 +0530

    only available train to be displayed

--- dvm_r2/myapp/views.py
+++ dvm_r2/myapp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from .models import Train
 
 def home(request):
-    trains_list = Train.objects.all()
-    paginator = Paginator(trains_list, 4)  # Show 7 trains per page
+    trains_list = Train.objects.filter(train_available=True)
+    paginator = Paginator(trains_list, 4) 
     page = request.GET.get('page')
     trains = paginator.get_page(page)
     return render(request, 'myapp/index.html', {'trains': trains})
