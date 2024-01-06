71e6d3cc6cac1f520fdc3513c7dfd1ef2ef25a66
ayush-gupta-dev <ayushguptadev1@gmail.com>
2024-01-06 17:38:05 +0530

    added search destination b/w stations

--- dvm_r2/myapp/urls.py
+++ dvm_r2/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="myapp-home"),
-    path('train/',views.train_details,name="train_details")
+    path('train/',views.train_details,name="train_details"),
+    path('choose_train/',views.choose_train,name="choose_train"),
+    path('choose_train_list',views.choose_train_list,name="choose_train_list")
 ]
--- dvm_r2/myapp/views.py
+++ dvm_r2/myapp/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Train, Station
+from django.db.models import F, Case, When, Value, Min, CharField
 
 def home(request):
     trains_list = Train.objects.filter(train_available=True)
@@ -20,6 +21,49 @@ def train_details(request):
         return render(request, 'myapp/train_details.html', {'train': train, 'stations': stations})
     else:
         return render(request, {'error_message': 'Please enter a valid train number'}, status=404)
-    
-# def custom_404(request, exception):
-#     return render(request, '404.html', status=404)
+
+
+def choose_train(request):
+    station_codes = Station.objects.values_list('station_code', flat=True).distinct()
+    #station_codes =   Station.objects.filter(station_code__isnull=False).distinct().distinct()
+    context = {'station_codes': station_codes}
+    return render(request, 'myapp/choose_train.html', context)
+
+# generated helper function from gpt based on sql query::
+# SELECT DISTINCT s1.train_number,s1.arrival_time as sourceStationTime, s2.arrival_time as DestinationStationTime
+# FROM myapp_station s1
+# INNER JOIN myapp_station s2 ON s1.train_number = s2.train_number
+# WHERE s1.station_code = 'ST3'
+#   AND s2.station_code = 'ST7'
+#   AND s1.arrival_time < s2.arrival_time;
+
+def get_relevant_train_number_and_time(source_station, destination_station):
+    relevant_trains = (
+        Station.objects
+        .filter(station_code=source_station)
+        .values('train__train_number','train__train_name' , 'train__total_seats_1ac','train__total_seats_2ac', 'train__total_seats_3ac', 'train__runs_on', 'train__fare_1ac','train__fare_2ac','train__fare_3ac','distance' ,'arrival_time','station_name')
+        .annotate(
+            destination_arrival_time=Min(
+                Case(
+                    When(train__station__station_code=destination_station, then=F('train__station__arrival_time')),
+                    default=Value('23:59:59'),  # Set a default time to ensure it is greater than any valid time
+                    output_field=CharField()  # You may need to adjust the output field type based on your model
+                )
+            )
+        )
+        .filter(destination_arrival_time__gt=F('arrival_time'))
+        .values('train__train_number', 'arrival_time', 'destination_arrival_time','train__train_name' , 'train__total_seats_1ac','train__total_seats_2ac', 'train__total_seats_3ac', 'train__runs_on', 'train__fare_1ac','train__fare_2ac','train__fare_3ac','distance','station_name')
+        .distinct()
+    )
+    return relevant_trains
+
+
+def choose_train_list(request):
+    if(request.method == 'POST'):
+        source_station = request.POST.get('source_station', '')
+        destination_station = request.POST.get('destination_station','')
+        relevant_trains = get_relevant_train_number_and_time(source_station,destination_station)
+        station_codes = Station.objects.values_list('station_code', flat=True).distinct()
+        context= {'relevant_trains':relevant_trains, 'stations_codes':station_codes,'source_station':source_station,'destination_station':destination_station}
+
+    return render(request,'myapp/choose_train_list.html',context)
