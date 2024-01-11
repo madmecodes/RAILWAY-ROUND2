bcd5ff5366ccadb1e20ed5696cc210885dc77fd4
ayush-gupta-dev <ayushguptadev1@gmail.com>
2024-01-12 00:47:57 +0530

    on cancellation update total train seat acc.

--- dvm_r2/booking/admin.py
+++ dvm_r2/booking/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Passenger
 from import_export.admin import ImportExportModelAdmin
 from .resources import PassengerResource
+from myapp.models import Train
 # Register your models here.
 
 @admin.register(Passenger)
@@ -9,11 +10,38 @@ class PassengerAdmin(ImportExportModelAdmin):
     resource_class = PassengerResource
 
     def save_model(self, request, obj, form, change):
-        print("Save model method called")
         cancel_status = obj.cancel_status
+        super().save_model(request, obj, form, change)
         if cancel_status:
             obj.user.profile.wallet_balance += obj.fare
             obj.user.profile.save()
+            try:
+                train = Train.objects.get(train_number=obj.train_number)
+            except Train.DoesNotExist:
+                train = None
+            if train:
+                if obj.ticket_type == '1AC':
+                    train.total_seats_1ac += 1
+                elif obj.ticket_type == '2AC':
+                    train.total_seats_2ac += 1
+                elif obj.ticket_type == '3AC':
+                    train.total_seats_3ac += 1
+                train.save()
+        if cancel_status==False:
+            obj.user.profile.wallet_balance -= obj.fare
+            obj.user.profile.save()
             print(f"Wallet balance updated: {obj.user.profile.wallet_balance}")
+            try:
+                train = Train.objects.get(train_number=obj.train_number)
+            except Train.DoesNotExist:
+                train = None
+            if train:
+                if obj.ticket_type == '1AC':
+                    train.total_seats_1ac -= 1
+                elif obj.ticket_type == '2AC':
+                    train.total_seats_2ac -= 1
+                elif obj.ticket_type == '3AC':
+                    train.total_seats_3ac -= 1
+                train.save()
 
 
